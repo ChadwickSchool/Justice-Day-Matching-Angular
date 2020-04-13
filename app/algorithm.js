2706b9a5028a1fbfd2e8fd50ac26d352b38da256
Michael Huang <mhuang2020@chadwickschool.org>
2020-04-13 09:40:28 -0700

    feat: implement working alg for single session

--- app/algorithm.js
+++ app/algorithm.js
@@ -75,18 +75,16 @@ async function getPopScore(projName){
 
 
 async function updateRawScore(){
-    let projects = projectdb.where("sessionNumber", "==", 2);
+    let projects = projectdb;
     await projects.get().then(function (querySnapshot) {
         querySnapshot.forEach(async function (doc) {
             let proj = doc.data();
-            console.log(proj.projectName);
-            // proj.rawScore = await getRawScore(proj.projectName);
-            let project = projectdb.doc(proj.projectName);
+            let project = projectdb.doc(doc.id);
             project.update({
                 rawScore: await getRawScore(proj.projectName)
             })
             .then(function(){
-                console.log(proj.projectName + " updated");
+                console.log(proj.projectName + " raw score updated");
             })
             .catch(function(error){
                 console.error("Error updating document: ", error);
@@ -95,92 +93,194 @@ async function updateRawScore(){
     });
 }
 
-updateRawScore();
+// updateRawScore();
 
-// function updatePopScore(){
-//     let projects = projectdb;
-//     projects.get().then(function (querySnapshot) {
-//         querySnapshot.forEach(function (proj) {
-//             let projPScore = getPopScore(proj.projectName);
-//             proj.popScore = projPScore;
-//             projectdb.doc(proj.id).update(proj);
-//         });
-//     });
-// }
+async function updatePopScore(){
+    let projects = projectdb;
+    await projects.get().then(function (querySnapshot) {
+        querySnapshot.forEach(async function (doc) {
+            let proj = doc.data();
+            let project = projectdb.doc(doc.id);
+            project.update({
+                popScore: await getPopScore(proj.projectName)
+            })
+            .then(function(){
+                console.log(proj.projectName + " pop score updated");
+            })
+            .catch(function(error){
+                console.error("Error updating document: ", error);
+            });
+        });
+    });
+}
 
-// function getUnderfilledGroups(){
-//     let underfilledProjects:string[] = [];
-//     let projects = projectdb.orderBy('rawScore');
-//     projects = projectdb.where('rawScore', '<=', minGroupSize);
-//     projects.get().then(function (querySnapshot) {
-//         querySnapshot.forEach(function (proj) {
-//             underfilledProjects.push(proj.projectName);
-//         });
-//     });
-//     return underfilledProjects;
-// }
+// updatePopScore();
+
+async function getUnderfilledGroups(){
+    let underfilledProjects = [];
+    let projects = projectdb.orderBy('rawScore');
+    projects = projectdb.where('rawScore', '<=', minGroupSize);
+    let querySnapshot = await projects.get();
+    querySnapshot.forEach(async function (doc) {
+        proj = doc.data();
+        underfilledProjects.push(proj.projectName);
+    });
+    return underfilledProjects;
+}
+
+async function giveAllPrefs(){
+//method to fill all groups with a raw score less than the required amount, starting from lowest score to highest
+    let underfilledProjs = await getUnderfilledGroups();
+    let j = underfilledProjs.length;
+    console.log('underfilled length: ' + j);
+    // gets all projects with raw score < min group size (var declared at top)
+    for(let i = 0; i<j;i++){
+        let projName = underfilledProjs[i];
+        console.log('current project ' + projName);
+        let proj = await projectdb.where('projectName', '==', projName);
+        let projectGet = await proj.get();
+        let projectStudents = [];
+        let currentId;
+
+        await projectGet.forEach(async (item) => {
+            let data = await item.data();
+            currentId = item.id;
+            projectStudents = projectStudents.concat(data.students);
+        });
+        let prefs = await prefdb.where('ranking', 'array-contains', projName);
+        prefs.get().then(function (querySnapshot) {
+            querySnapshot.forEach(async function (doc) {
+            //for every project in underfilled project, find specific preference doc to find student and set student to matched
+                let pref = doc.data();
+                let studId = pref.uid;
+                await studentdb.doc(studId).get().then(async function (doc) {
+                    let student = await doc.data();
+                    if(student.matched == false){
+                        let studentObj = studentdb.doc(doc.id);
+
+                        studentObj.update({
+                            matched: true
+                        })
+                        .then(function(){
+                            console.log(student.name + " matched");
+                        })
+                        .catch(function(error){
+                            console.error("Error matching student: ", error);
+                        });
+                        projectStudents.push(student.name);
+                        let project = projectdb.doc(currentId);
+                        project.update({
+                            students: projectStudents
+                        })
+                        .then(function(){
+                            console.log(student.name + " matched to project " + projName);
+                        })
+                        .catch(function(error){
+                            console.error("Error matching student: ", error);
+                        });
+                    }
+                });
+            });
+        });
+    }
+}
 
 
-// function giveAllPrefs(){
-// //method to fill all groups with a raw score less than the required amount, starting from lowest score to highest
-//     let underfilledProjs = getUnderfilledGroups();
-//     let j = underfilledProjs.length;
-//     //gets all projects with raw score < min group size (var declared at top)
-//     for(let i = 0; i<j;i++){
-//         let projName = underfilledProjs[i];
-//         let proj = projectdb.doc('projectName', '==', projName);
-//         let prefs = prefdb.where('rankings', 'array-contains', projName);
-//         prefs.get().then(function (querySnapshot) {
-//             querySnapshot.forEach(function (pref) {
-//             //for every project in underfilled project, find specific preference doc to find student and set student to matched
-//                 let studId = pref.uid;
-//                 studentdb.doc('uid', '==', studId).get().then(function (student) {
-//                     let studentName = student.name;
-//                     student.matched = true; 
-//                     studentdb.doc(student.id).update(student);
-//                     proj.get.then(function (project) {
-//                     //add each student name to list of students for each project
-//                         project.students.push(studentName);
-//                         projectdb.doc(project.id).update(project);
-//                     });
-//                 });
-//             });
-//         });
-//     }
-// }
+async function giveNthPrefs(n){
+//method to give all nth preferences of students until max limit is reached, starting from lowest popularity score to highest
+    let projects = await projectdb.orderBy('popScore');
+    //list of projects sorted by lowest popularity score
+    await projects.get().then(function (querySnapshot){
+        querySnapshot.forEach(async function (doc){
+            let project = await doc.data();
+            let currentId = await doc.id;
+            let projectName = project.projectName;
+            console.log("current project: " + projectName);
+            let projectStudents = project.students; 
+            console.log("current groupsize: " + projectStudents.length)
+                        //loop through every project and find prefs have ranked project as nth
+                let prefs = await prefdb.where('ranking', "array-contains", projectName);
+                let nthPrefs = [];
+                await prefs.get().then(function (querySnapshot){
+                    querySnapshot.forEach(async function (doc){
+                        let pref = await doc.data();
+                        if(pref.ranking[n] == projectName){
+                            nthPrefs.push(pref.uid);
+                        }
+                    });
+                });
+                for(let i = 0; i < await nthPrefs.length; i++){
+                    let studId = nthPrefs[i];
+                    await studentdb.doc(studId).get().then(async function (doc) {
+                        let student = await doc.data()
+                        if(student.matched == false && projectStudents.length < maxGroupSize){
+                            let studentObj = await studentdb.doc(doc.id);
+                            studentObj.update({
+                                matched: true
+                            })
+                            .then(function(){
+                                console.log(student.name + " matched");
+                            })
+                            .catch(function(error){
+                                console.error("Error matching student: ", error);
+                            });
+                            projectStudents.push(student.name);
+                            let project = await projectdb.doc(currentId);
+                            project.update({
+                                students: projectStudents
+                            })
+                            .then(function(){
+                                console.log(student.name + " matched to project " + " as preference: " + n);
+                            })
+                            .catch(function(error){
+                                console.error("Error matching student: ", error);
+                            });
+                        }   
+                    });
+                }
+        });
+    });
+}
 
-// function giveNthPrefs(n){
-// //method to give all nth preferences of students until max limit is reached, starting from lowest popularity score to highest
-//     let projects = projectdb.orderBy('popScore');
-//     //list of projects sorted by lowest popularity score
-//     projects.get().then(function (querySnapshot){
-//         querySnapshot.forEach(function (project){
-//             let projectName = project.projectName;
-//             if(project.students.length < maxGroupSize){
-//                         //loop through every project and find prefs have ranked project as nth
-//                 let prefs = prefdb.where('matched', "==", false);
-//                 let nthPrefs = prefs.filter(function (p){
-//                     return p.rankings[n] == projectName;
-//                 });
-//                 for(let i = 0; i < nthPrefs.length; i++){
-//                     let pref = nthPrefs[i];
-//                     let studId = pref.uid;
-//                     studentdb.doc('uid', '==', studId).get().then(function (student) {
-//                         if(student.matched == false){
-//                             let studentName = student.name;
-//                             student.matched = true; 
-//                             studentdb.doc(student.id).update(student);
-//                             project.get.then(function (project) {
-//                             //add each student name to list of students for each project
-//                             project.students.push(studentName);
-//                             projectdb.doc(project.id).update(project);
-//                             pref.matched = true;
-//                             prefdb.doc(pref.id).update(pref);
-//                             });
-//                         }   
-//                     });
-//                 }
-//             }
-//         });
-//     });
+async function clearProjects(){
+    await projectdb.get().then(function(querySnapshot) {
+        querySnapshot.forEach(async function (doc) {
+            let project = await projectdb.doc(doc.id);
+            project.update({
+                students: []
+            })
+            .then(function(){
+                console.log("project cleared: " + doc.data().projectName);
+            })
+            .catch(function(error){
+                console.error("Error clearing project: ", error);
+            });
+        });
+    });
+}
+
+async function setMatchedFalse(){
+    await studentdb.get().then(function(querySnapshot) {
+        querySnapshot.forEach(async function (doc) {
+            let student = await studentdb.doc(doc.id);
+            student.update({
+                matched: false
+            })
+            .then(function(){
+                console.log("Student unmatched: " + doc.data().name);
+            })
+            .catch(function(error){
+                console.error("Error unmatching student:  ", error);
+            });
+        });
+    });
+}
+
+// clearProjects();
+// setMatchedFalse();
+// updateRawScore();
+// updatePopScore();
+// giveAllPrefs();
+// for(let i = 0; i < 4; i++){
+//     giveNthPrefs(i);
 // }
